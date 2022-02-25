b371430122bf736ac37d52ea6a7f190988adc590
Bridgett Wheeler <bridgettwheeler@gmail.com>
2022-02-25 16:43:08 -0500

    commit tree 9f81ac12ed183d788d0e4bb2a2d01a36a8f811bb

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 50, 'M', 'brown', 'hangry', 'yes');
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'orange', 'hyper', 'yes');
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 27, 'F', 'tan', 'sassy', 'yes');
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 90, 'M', 'white', 'grumpy', 'no');
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 45, 'F', 'yellow', 'happy', 'yes');
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 40, 'M', 'green', 'psycotic', 'yes');
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 25, 'F', 'red', 'friendly', 'yes');
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'F', 'blue', 'curious', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 50, 'M', 'brown', 'hangry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'orange', 'hyper', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 27, 'F', 'tan', 'sassy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 90, 'M', 'white', 'grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 45, 'F', 'yellow', 'happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 40, 'M', 'green', 'psycotic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 25, 'F', 'red', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'F', 'blue', 'curious', 1);
