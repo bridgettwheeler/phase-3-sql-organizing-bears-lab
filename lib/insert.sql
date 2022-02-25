c314b987087b6715ca2e3c19421d01cca1382d8d
Bridgett Wheeler <bridgettwheeler@gmail.com>
2022-02-25 11:40:38 -0500

    stopping point

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 50, 'M', 'brown', 'hangry', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'orange', 'hyper', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 27, 'F', 'tan', 'sassy', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 90, 'M', 'white', 'grumpy', 'no');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 45, 'F', 'yellow', 'happy', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 40, 'M', 'green', 'psycotic', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 25, 'F', 'red', 'friendly', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'F', 'blue', 'curious', 'yes');
